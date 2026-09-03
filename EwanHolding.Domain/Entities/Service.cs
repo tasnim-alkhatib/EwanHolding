8054ac3e8495e21958e2d157bd7c22893e57f577
Tasnim <tasnim201050@gmail.com>
2026-09-03 15:50:42 +0300

    Add Service Table

---
+++ EwanHolding.Domain/Entities/Service.cs
@@ -0,0 +1,14 @@
+﻿using EwanHolding.Domain.Common;
+
+namespace EwanHolding.Domain.Entities
+{
+    public class Service : BaseEntity
+    {
+        public string Name_Ar { get; set; }
+        public string Name_En { get; set; }
+        public string Description_Ar { get; set; }
+        public string Description_En { get; set; }
+
+        public int CompanyId { get; set; }
+    }
+}
