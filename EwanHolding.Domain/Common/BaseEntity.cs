38315a094ad2b5c6255b7e3a6ca8858771e20ffc
Tasnim <tasnim201050@gmail.com>
2026-09-03 13:40:37 +0300

    Add project files.

---
+++ EwanHolding.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace EwanHolding.Domain.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
